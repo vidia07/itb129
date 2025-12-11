62eef09654f672b80938f75416affc734384f24a
faris180427 <farisgibran1804@gmail.com>
2025-12-11 01:01:48 -0800

    faris

---
+++ Chapter08/714250029_Faris/php.php
@@ -0,0 +1,16 @@
+<?php
+echo "Hello, World!";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HTML Pemula/title>
+</head>
+<body>
+     <h1>Hello, World!<?php echo " from PHP"; ?></h1>
+    
+</body>
+</html>
