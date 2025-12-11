f66a71fef8e4b9bc786e2caeab8d0467c41bc806
Rhaya Wibawareksa <rhaja.agung7@gmail.com>
2025-12-11 16:16:57 +0700

    rhaya beres easy

---
+++ Chapter08/714250029_Rhaya/operatorlogika.php
@@ -0,0 +1,14 @@
+<?php
+    $bil1 = 100;
+    $bil2 = 20;
+
+    $teks1 = "PHP";
+    $teks2 = "php";
+
+    $hasil = ($bil1 < $bil2) or ($teks1 == $teks2);
+    printf("($bil1 < $bil2) or ($teks1 == $teks2) adalah %d <BR>\n",
+           $bil1, $bil2, $teks1, $teks2, $hasil);
+
+    $hasil = !($teks1 == $teks2);
+    printf("! (%s == %s) adalah %d <BR>\n", $teks1, $teks2, $hasil);
+?>
---
+++ Chapter08/714250029_Rhaya/printf.php
@@ -0,0 +1,4 @@
+<?php
+    $txt = "kampus digital masa gitu";
+    printf("Aku belajar pemrograman PHP di %s <br>", $txt);
+?>
