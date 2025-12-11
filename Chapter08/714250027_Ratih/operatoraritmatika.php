4699118bd06bcd7fdc2e81de69074bb8483b57af
ratihnurfaida <ratihnurfaida@gmail.com>
2025-12-11 15:29:05 +0700

    latihanphpratih

---
+++ Chapter08/714250027_Ratih/operatoraritmatika.php
@@ -0,0 +1,16 @@
+<?php
+    $a = 1;
+    $b = 2;
+    $c = 3;
+    $s1 = "Hallo";
+    print ("Penjumlahan dan Pengurangan :<br> \n");
+    printf ("$a + $b = %d <br> \n", $a + $b);
+    printf ("$b - $a = %d <br> \n", $c - $a);
+    print ("<br> Perkalian : <br> \n");
+    printf ("$c * $b = %d <br> \n", $c * $b);
+    printf ("$c * 0.5 = %d <br> \n", $c * 0.5);
+    print ("<br> Pembagian : <br> \n");
+    printf ("$c / $b = %d <br> \n", $c / $b);
+    printf ("$c / 3.5 = %d <br> \n", $c / 3.5);
+    print ("<br> Yang ini menampilkan variabel String = $s1 ");
+?>
