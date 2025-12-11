35bfb002218c66df52a5d31b6a061a1d96d26507
muhammadsalmanafaris12-cpu <muhammadsalmanafaris12@gmail.com>
2025-12-11 15:23:14 +0700

    salmanchapter8

---
+++ Chapter08/714250031_MuhammadSalmanAfaris/index2.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP</title>
+</head>
+<body>
+    <!-- <h1>
+        Hello ,selamat datang <?php echo "Roni"?>
+    </h1> -->
+
+    <h1>Hello , selamat datang <?php echo "Roni" ?></h1>
+</body>
+</html>
