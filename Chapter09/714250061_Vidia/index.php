f001865c3f62af31c172402da060b51bde808989
vidia07 <vidiasafira73@gmail.com>
2025-12-24 14:01:24 +0700

    vidia

---
+++ Chapter09/714250061_Vidia/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-wosth, initial-scale=1.0">
+    <tittle>Pendaftaran Mahasiswa Baru POLTELPOS</tittle>
+</head>
+<body>
+    <header>
+        <h3>Pendaftaran Mahasiswa Baru</h3>
+        <h1>politeknik Pos Indonesia</h1>
+    </header>
+
+    <h4>Menu</h4>
+    <nav>
+        <ul>
+            <li><a href="form-daftar.php">Daftar</a></li>
+            <li><a href="list-maba.php">Pendaftar</a></li>
+        </ul>
+    </nav>
+
+</body>
+</html>
